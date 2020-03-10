374dc69856dbc99231d92bcb7dd893dbdbcb65f5
nabila <nabilatajrin@gmail.com>
2020-03-10 10:03:20 +0600

    10:04AM 10MAR2020 update

---
+++ basics/operators-and-branching.py
@@ -0,0 +1,12 @@
+print(3 > 4)
+print(4.0 > 3.999)
+print(4 > 4)
+print(4 > + 4)
+print(+4) #positive integer
+print(-4)
+print(True or False)
+print(False or False)
+print(not False)
+print(3.0 - 1.0 != 5.0 - 3.0)
+print(3 > 4 or (2 < 3 and 9 > 10))
+print(4 > 5 or 3 < 4 and 9 > 8)
---
+++ core-elements-of-programs/control-flow-loops.py
@@ -0,0 +1,10 @@
+# more complicated with while loop
+n = 0
+while n < 5:
+    print(n)
+    n = n+1
+
+
+# shortcut with for loop
+for n in range(5):
+    print(n)
