de6e7ae0517aafe81d1f2438a5eb595f5a1aba11
nabila <nabilatajrin@gmail.com>
2020-03-11 12:24:38 +0600

    12:24PM 11MAR2020 update

---
+++ problem-set-week-01/iteration-05.py
@@ -0,0 +1,7 @@
+count = 0
+phrase = "hello, world"
+for iteration in range(5):
+    while True:
+        count += len(phrase)
+        break
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
