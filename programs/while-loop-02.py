80c5df16ea8403ba3f8dce6cffd6f66c2a58fd0a
nabila <nabilatajrin@gmail.com>
2020-03-11 10:45:16 +0600

    10:47AM 11MAR2020 update

---
+++ programs/while-loop-02.py
@@ -0,0 +1,8 @@
+print("Hello!")
+
+num = 10
+while num >= 2:
+    print(num)
+    num -= 2
+
+
---
+++ programs/while-loop-sum.py
@@ -0,0 +1,10 @@
+end = int(input("type end number: "))
+x = 0
+n = 1
+while n <= end:
+    print(n)
+    x = x + n
+    n += 1
+
+print("Total:")
+print(x)
