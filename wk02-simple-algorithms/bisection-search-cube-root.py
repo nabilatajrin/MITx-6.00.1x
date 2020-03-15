96901c5d8a85a6fdf25729e48baef0cfd9834291
nabila <nabilatajrin@gmail.com>
2020-03-15 16:52:06 +0600

    04:52PM 15MAR2020 update

--- wk02-simple-algorithms/bisection-search-cube-root.py
+++ wk02-simple-algorithms/bisection-search-cube-root.py
@@ -9,7 +9,7 @@ while abs(guess**3 - cube) >= epsilon:
         low = guess
     else:
         high = guess
-        guess = (high + low)/2.0
-        num_guesses += 1
+    guess = (high + low)/2.0
+    num_guesses += 1
 print('num_guesses =', num_guesses)
 print(guess, 'is close to the cube root of', cube)
