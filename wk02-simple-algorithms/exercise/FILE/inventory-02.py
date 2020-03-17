b0bb29e462fbefab4155cb3b4463afebfdcd3e03
nabila <nabilatajrin@gmail.com>
2020-03-17 22:10:27 +0600

    10:10PM 17MAR2020 update

---
+++ wk02-simple-algorithms/exercise/FILE/inventory-02.py
@@ -0,0 +1,5 @@
+from batteries import *
+aa = "aa"
+print(aa, aaa, c, d)
+
+# working but showing error
---
+++ wk02-simple-algorithms/exercise/FILE/inventory.py
@@ -0,0 +1,7 @@
+import batteries
+
+aa = "aa"
+tripleA = "aaa"
+print(batteries.aa)
+
+# working but showing error
