51a23dc84a5c02a2a2b052c49a8ca972c520f081
nabila <nabilatajrin@gmail.com>
2020-03-26 21:18:50 +0600

    09:18PM 26MAR2020 update

---
+++ wk03-structured-types/dictionaries/function-to-analyze-songs-lyrics.py
@@ -0,0 +1,20 @@
+def lyrics_to_frequencies(lyrics):
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
+
+
+def most_common_words(freqs):
+    values = freqs.values()
+    best = max(values)
+    words = []
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return (words, best)
+
+print(lyrics_to_frequencies('twinkle twinkle little star'))
---
+++ wk03-structured-types/dictionaries/keys-and-values.py
@@ -0,0 +1,4 @@
+d = {4:{1:0}, (1,3):"twelve", 'const':[3.14,2.7,8.44]}
+
+print(d.keys())
+print(d.values())
